28d58a1ddb25560d13730d16039c9be6691aa07f
Diane <diane.maniere@gmail.com>
2023-07-23 12:40:54 +0200

    Use elixir releases

---
+++ config/releases.exs
@@ -0,0 +1,2 @@
+# Newer versions of Elixir use a runtime.exs file instead of releases.exs. Gigalixir’s buildpacks will want a releases.exs to determine this is not a Mix deployment.
+# This file can be empty, it just needs to exist.
